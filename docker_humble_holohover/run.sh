da41e4909415721415c314371638a30d6fa54cd4
MatasJones <126103079+MatasJones@users.noreply.github.com>
2025-03-02 12:35:53 +0100

    created latency test folder

--- docker_humble_holohover/run.sh
+++ docker_humble_holohover/run.sh
@@ -9,20 +9,29 @@ parent_dir=$(dirname "$current_dir")
 IMAGE_NAME="latency_test_image"
 CONTAINER_NAME="latency_test_container"
 
-# Stop and remove any images with the same name to avoid conflicts
+USERNAME=holohover
+GROUPNAME=holohover  # Group name to be used
+
+# Stop and remove any containers with the same name to avoid conflicts
 docker stop $CONTAINER_NAME 2>/dev/null || true
 docker rm $CONTAINER_NAME 2>/dev/null || true
 
-# Select docker container
-docker run -it \
-    --name latency_test \
-    --rm \
-    --privileged \
-    --net=host \
-    -v /dev:/dev \
-
-
+# Select Docker container parameters
 # --rm is used to automatically remove the container when it is closed
 # --privileged is used to allow the container to have access to the hardware devices
-# --net=host allows the contain to use the host's network
-# -v /dev:/dev gives the container access to all of the hardware
+# --net=host allows the container to use the host's network
+# -v /dev:/dev gives the container access to all hardware devices
+
+docker run -it \
+    --name $CONTAINER_NAME \
+    --rm \
+    --privileged \
+    --hostname $USERNAME \
+    --net=host \
+    -v /dev:/dev \
+    -e USERNAME=$USERNAME \
+    mj_holohover \
+
+
+
+
