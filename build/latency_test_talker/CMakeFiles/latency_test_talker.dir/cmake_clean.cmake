8c0198868a1db4005cdec947b7693073b4d3643e
MatasJones <126103079+MatasJones@users.noreply.github.com>
2025-03-07 09:10:58 +0100

    fix: changed name to latency_test_talker

---
+++ build/latency_test_talker/CMakeFiles/latency_test_talker.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/latency_test_talker.dir/src/latency_test_main.cpp.o"
+  "CMakeFiles/latency_test_talker.dir/src/latency_test_main.cpp.o.d"
+  "CMakeFiles/latency_test_talker.dir/src/talker.cpp.o"
+  "CMakeFiles/latency_test_talker.dir/src/talker.cpp.o.d"
+  "latency_test_talker"
+  "latency_test_talker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/latency_test_talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
