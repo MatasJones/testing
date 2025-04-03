bccc67418edcac6f5a37d92183d6eff5178be43d
MatasJones <126103079+MatasJones@users.noreply.github.com>
2025-04-03 15:30:13 +0200

    feat: added main remote launch script

---
+++ docker_humble_holohover/main_docker_remote_launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the current working directory
+current_dir=$(pwd)
+
+# Use dirname to get the parent directory
+parent_dir=$(dirname "$current_dir")
+
+CMD="docker run -dt --rm --privileged --network host -v $parent_dir:/home/testing/dev_ws/src --name main_testing_container main_testing_image"
+
+bash -c "$CMD"
