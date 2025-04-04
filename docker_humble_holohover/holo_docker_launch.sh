a4d25d96f319402da72efd64afdbada74b0a5d5a
MatasJones <126103079+MatasJones@users.noreply.github.com>
2025-04-04 15:12:33 +0200

    fix: launch docker and ros launch sequence 15...

--- docker_humble_holohover/holo_docker_launch.sh
+++ docker_humble_holohover/holo_docker_launch.sh
@@ -9,7 +9,7 @@ parent_dir=$(dirname "$current_dir")
 CONTAINER_NAME="holo_testing_container"
 
 # Run container in detached mode, keep it alive using 'tail -f /dev/null'
-docker run -d --privileged --network host --name "$CONTAINER_NAME" holo_testing_image tail -f /dev/null
+docker run -it --privileged --network host --name "$CONTAINER_NAME" holo_testing_image tail -f /dev/null
 
 # Capture the container ID for cleanup
 CONTAINER_ID=$(docker ps -q -f name="$CONTAINER_NAME")
