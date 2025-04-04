726a6f15decce47f5e241c6605e6d75fb122e2cf
MatasJones <126103079+MatasJones@users.noreply.github.com>
2025-04-04 13:56:09 +0200

    fix: launch docker and ros launch sequence

--- docker_humble_holohover/holo_docker_launch.sh
+++ docker_humble_holohover/holo_docker_launch.sh
@@ -10,6 +10,13 @@ CMD="docker run -it --rm --privileged --network host --name holo_testing_contain
 
 bash -c "$CMD"
 
+CONTAINER_NAME=holo_testing_container
+
+until [ "$(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME 2>/dev/null)" == "true" ]; do
+    echo "Waiting for container '$CONTAINER_NAME' to be running..."
+    sleep 1
+done
+
 CMD="docker exec -it holo_testing_container bash -c 'source /home/testing/dev_ws/install/setup.bash && ros2 launch latency_test_listener listener_launch.py'"
 
 bash -c "$CMD"
--- docker_humble_holohover/main_docker_launch.sh
+++ docker_humble_holohover/main_docker_launch.sh
@@ -10,6 +10,13 @@ CMD="docker run -it --rm --privileged --network host -v $parent_dir/testing_logs
 
 bash -c "$CMD"
 
+CONTAINER_NAME=main_testing_container
+
+until [ "$(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME 2>/dev/null)" == "true" ]; do
+    echo "Waiting for container '$CONTAINER_NAME' to be running..."
+    sleep 1
+done
+
 CMD="docker exec -it main_testing_container bash -c 'source /home/testing/dev_ws/install/setup.bash && ros2 launch latency_test_talker talker_launch.py'"
 
 bash -c "$CMD"
