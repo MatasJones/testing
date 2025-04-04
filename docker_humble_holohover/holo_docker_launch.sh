b07b6030cd4f2f7f5bc4abd799c02c1c70a2ba73
MatasJones <126103079+MatasJones@users.noreply.github.com>
2025-04-04 14:00:03 +0200

    fix: launch docker and ros launch sequence 2

--- docker_humble_holohover/holo_docker_launch.sh
+++ docker_humble_holohover/holo_docker_launch.sh
@@ -6,17 +6,21 @@ current_dir=$(pwd)
 # Use dirname to get the parent directory
 parent_dir=$(dirname "$current_dir")
 
-CMD="docker run -it --rm --privileged --network host --name holo_testing_container holo_testing_image"
+# CMD="docker run -it --rm --privileged --network host --name holo_testing_container holo_testing_image"
+CMD="docker run -it --rm --privileged --network host --name holo_testing_container \
+    holo_testing_image \
+    bash -c "source /home/testing/dev_ws/install/setup.bash && ros2 launch latency_test_listener listener_launch.py""
+
 
 bash -c "$CMD"
 
-CONTAINER_NAME=holo_testing_container
+# CONTAINER_NAME=holo_testing_container
 
-until [ "$(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME 2>/dev/null)" == "true" ]; do
-    echo "Waiting for container '$CONTAINER_NAME' to be running..."
-    sleep 1
-done
+# until [ "$(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME 2>/dev/null)" == "true" ]; do
+#     echo "Waiting for container '$CONTAINER_NAME' to be running..."
+#     sleep 1
+# done
 
-CMD="docker exec -it holo_testing_container bash -c 'source /home/testing/dev_ws/install/setup.bash && ros2 launch latency_test_listener listener_launch.py'"
+# CMD="docker exec -it holo_testing_container bash -c 'source /home/testing/dev_ws/install/setup.bash && ros2 launch latency_test_listener listener_launch.py'"
 
-bash -c "$CMD"
+# bash -c "$CMD"
